31a24b95e0614bf18c37b5dca3ccc5b7e4dba380
SolindekDev <83218819+SolindekDev@users.noreply.github.com>
2023-05-13 09:35:56 +0200

    Create install_ubuntu.sh

---
+++ scripts/install_ubuntu.sh
@@ -0,0 +1,12 @@
+echo "Installing netlore dependencies"
+echo "Installing gcc"
+sudo apt-get install -y gcc
+echo "Installing curl"
+sudo apt-get install -y libcurl-dev
+echo "Installing SDL2"
+sudo apt-get install -y libsdl2-dev
+echo "Installing SDL2_image"
+sudo apt-get install -y libsdl2-image-dev
+echo "Installing SDL2_ttf"
+sudo apt-get install -y libsdl2-ttf-dev
+echo "Done."
