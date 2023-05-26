cd06dffcb8497dbb2d9220d12569edb74fba7068
SachaTending <65766775+SachaTending@users.noreply.github.com>
2023-05-26 12:34:37 +0000

    Fix build error

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -2,7 +2,7 @@ echo "Installing netlore dependencies"
 echo "Installing gcc"
 sudo apt-get install -y gcc
 echo "Installing curl"
-sudo apt-get install -y libcurl4-openssl-dev libcurl4-nss-dev libcurl4-gnutls-dev
+sudo apt-get install -y libcurl4-openssl-dev libcurl4-nss-dev libcurl4-gnutls-dev libcurl-dev
 echo "Installing SDL2"
 sudo apt-get install -y libsdl2-dev
 echo "Installing SDL2_image"
